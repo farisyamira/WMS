ac1562b4e8696ef38116e47fc1219c7c420bdc62
dianamaisarah37 <dianamaisarah37@gmail.com>
2025-06-11 21:35:09 +0800

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:wms/App/Pages/ManageShopInventory/AddInventoryForm.dart';
 import 'package:wms/App/Pages/ManageShopInventory/ShopInventory.dart';
 import 'package:wms/App/Pages/homepage.dart';
 import 'package:wms/App/pages/ManageShopInventory/AddInventoryForm.dart';
-import 'package:wms/App/pages/ManageShopInventory/InventoryDetail.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -46,7 +46,6 @@ class WMSApp extends StatelessWidget {
       routes: {
         '/inventory': (context) => const ShopInventoryPage(),
         '/add-item': (context) => const AddInventoryForm(),
-        '/item-detail': (context) => const InventoryDetail(item:),
       },
     );
   }
